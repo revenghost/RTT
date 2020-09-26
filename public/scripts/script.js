0d91ad053d2c8b8a3c13b818cce57fb56e136d86
RevenGhost <revenghost@gmail.com>
2020-09-26 16:46:06 -0600

    cleanup

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -4,14 +4,16 @@
 
 
  
-//  let currentdate = moment('2013-03-01', 'YYYY-MM-DD');
- 
- function alertSelected() {
-    // currentdate = moment().format("HH:mm");
-   // m = moment('2013-03-01', 'YYYY-MM-DD')
-    alert("Hello");
+var today = new Date();
+var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+ function alertSelected(btnname) {
+    alert(btnname + "\n" + dateTime);
 };
  
 
 clockInBtn.addEventListener("click",alertSelected);
 clockOutBtn.addEventListener("click",alertSelected);
+// clockOutBtn.addEventListener("click",alertSelected("Clocked Out"));
