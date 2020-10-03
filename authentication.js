30fcbbb09bc8d585715cc587c91c67468aa98fa5
RevenGhost <revenghost@gmail.com>
2020-10-03 13:39:02 -0600

    working on authentication.js

---
+++ authentication.js
@@ -0,0 +1,8 @@
+const credentials = { 
+    username: "RTTdbAdmin",
+    password: "JQu7FjCqqlnZDEuZ",
+    db: "RTTDB",
+    connectionurl: "@cluster0.kvs7o.mongodb.net/RTTDB?retryWrites=true&w=majority"
+}
+
+modules.export
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,12 +1,13 @@
 const db = require("../models");
 const path = require("path");
 module.exports = function(app){
-app.get("/alert", function(req, res){
-res.sendfile(path.join(__dirname, "../public/pages/timeCard.html"));
-
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "./index.html"));
 });
 
-
+app.get("/timeCard", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/pages/timeCard.html"));
+});
 
 
 
